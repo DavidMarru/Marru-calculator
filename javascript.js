c95a77851722b6a69ae17e7b7f1d3b6acbd04d83
David Marrucate <144349276+DavidMarru@users.noreply.github.com>
2023-12-05 17:26:55 +0000

    updated display info

--- javascript.js
+++ javascript.js
@@ -3,18 +3,25 @@ let currentOperator = '';
 let firstOperand = '';
 let result = '0';
 let decimalAdded = false;
+let equation = '';
+let shouldCalculate = false;
 
 function appendNumber(number) {
     currentInput += number;
+    equation += number;
     updateDisplay();
+    console.log(`History:`, equation);
+    document.getElementById('equationDisplay').value = equation;
     console.log('Appended Number:', number);
 }
 
 function appendDecimal() {
     if (!decimalAdded) {
         currentInput += '.';
+        equation += `.`;
         decimalAdded = true; 
         updateDisplay();
+        document.getElementById('equationDisplay').value = equation;
         console.log('Appended Decimal');
     }
 }
@@ -31,12 +38,22 @@ function appendOperator(operator) {
             currentOperator = operator;
         }
         decimalAdded = false;  
+        equation += operator;
+        document.getElementById('equationDisplay').value = equation;
         console.log('Appended Operator:', operator);
     }
 }
 
+function calculateLoop() {
+    shouldCalculate = true;
+    calculate();
+}
+
 function calculate() {
-    if (currentInput !== '' && firstOperand !== '') {
+    if (shouldCalculate && currentOperator !== '' && currentInput !== '') {
+        equation = `${equation} =`;
+        document.getElementById('equationDisplay').value = equation;
+
         const operand1 = parseFloat(result);
         const operand2 = parseFloat(currentInput);
 
@@ -59,24 +76,26 @@ function calculate() {
         decimalAdded = false;  
         updateDisplay();
         console.log('Calculated Result:', result);
+        shouldCalculate = false;
     }
-}
-
-function ongoingEquation(equation) {
-
 }
 
 function clearDisplay() {
     firstOperand = '';
     currentInput = '';
     currentOperator = '';
+    equation = '';
     result = '0';
-    decimalAdded = false;  
+    decimalAdded = false; 
+    shouldCalculate = false;
     updateDisplay();
     console.log('Cleared display');
+    document.getElementById('equationDisplay').value = equation;
+
 }
 
 function updateDisplay() {
-    document.getElementById('mainDisplay').value = result ;
-    document.getElementById('equationDisplay').value = `${currentOperator} ${currentInput}`;
+    document.getElementById('mainDisplay').value = result;
 }
+
+document.getElementById('equalsButton').addEventListener('click', calculateLoop);
